2c1f9bdc79a0383c4e2cf78d7134a223988f5893
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-01 15:11:59 +0200

    setup https://github.com/y-neck/weathertunes/blob/3009b544a150af46bfb048f0aead6367f635564a/index.php

---
+++ frontend/components/layout.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8">
+    <!--Default character set: https://www.w3schools.com/html/html_charset.asp-->
+    <title>Weathertunes</title>
+    <!--Tab title-->
+    <meta name="description" content="">
+    <!--Description of web page-->
+    <meta name="keywords" content="weather, tunes, music, spotify, api">
+    <!--define keywords for search engines-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Responsive web design:https://www.w3schools.com/css/css_rwd_viewport.asp-->
+
+    <link rel="icon" href="">
+    <!--TODO: Tab image-->
+
+    <!--<link rel="stylesheet" href=".css">    <!--TODO: Insert CSS file-->
+</head>
--- index.php
+++ index.php
@@ -1,29 +1,6 @@
-<!DOCTYPE html>
-<html lang="de" dir="ltr">
-<!--Default language german, reading from l. to r.-->
-
-<head>
-    <meta charset="utf-8">
-    <!--Default character set: https://www.w3schools.com/html/html_charset.asp-->
-    <title></title>
-    <!--TODO: Tab title-->
-    <meta name="description" content="">
-    <!--Description of web page-->
-    <meta name="keywords" content="">
-    <!--define keywords for search engines-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Responsive web design:https://www.w3schools.com/css/css_rwd_viewport.asp-->
-
-    <link rel="icon" href="">
-    <!--TODO: Tab image-->
-
-    <!--<link rel="stylesheet" href=".css">    <!--TODO: Insert CSS file-->
-
-</head>
+<?php require_once './frontend/components/layout.php'; ?>
 
 <body>
-hello world this is a test
- 
-</body>
+    hello world this is a test
 
-</html>
+</body>
