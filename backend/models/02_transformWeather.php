073fbc49bfd1f729cbe24171eee800346076a27d
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-02 17:33:35 +0200

    transform weather, follow-up on https://github.com/y-neck/weathertunes/commit/21275cf3706241f7b29474250c5fa09db6faa864

--- backend/models/02_transformWeather.php
+++ backend/models/02_transformWeather.php
@@ -3,17 +3,19 @@ require_once '../models/01_extractWeather.php';
 
 // print_r($weatherdata);
 reduceWeatherData($weatherdata);
+
+
 print_r($weatherdata);
 
 
-function reduceWeatherData(&$data)
+function reduceWeatherData(&$data)  // Reduce data to relevant values
 {
     // Create a new array to store reduced data
     // Extract value dimensions from current data
     $data = [
-        'temperature' => $data['current']['temperature_2m'],
+        'temperature' => (int) round($data['current']['temperature_2m']),
         'isDay' => $data['current']['is_day'],
         'weatherCode' => $data['current']['weather_code'],
-        'windSpeed' => $data['current']['wind_speed_10m'],
+        'windSpeed' => (int) round($data['current']['wind_speed_10m']),
     ];
 }
