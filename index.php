bf8ea43ebe2389463dcfb3f97d84683af1afc872
Clatoeny <cla.toeny@hotmail.com>
2024-05-03 10:46:04 +0200

    index all boxes done

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ require_once './backend/db/db.config.php';
 <main>
     <div id="main-box" class="flex">
         <div id="top-box" class="flex-1">
-                <div id="status-box" class="m-2 p-4 border border-gray-200">Content 1A</div>
+            <div id="status-box" class="grid grid-rows-2 grid-cols-2 m-2 p-4 border border-gray-200 gap-2">
+                    <div id="weather-desc-box" class="col-span-2 bg-gray-200 p-2">Content 1A - Top</div>
+                    <div id="weather-icon-box" class="bg-gray-300 p-2">Content 1A - Bottom Left</div>
+                    <div id="flag-icon-box" class="bg-gray-300 p-2">Content 1A - Bottom Right</div>
+                </div>    
                 <div id="temp-box" class="m-2 p-4 border border-gray-200">Content 1B</div>
             </div>
             
@@ -16,7 +20,10 @@ require_once './backend/db/db.config.php';
                 <div id="slider-box" class="m-2 p-4 border border-gray-200">Content 2B</div>
             </div>
     </div>
-
+    <div id="spotify-container" class="m-2 p-4">
+        <!-- Placeholder for the Spotify iframe -->
+        <iframe id="spotify-iframe" src="https://open.spotify.com/embed/track/yourSpotifyTrackID" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </div>
 </main>
 <?php require_once './frontend/components/footer.php'; ?>
 </body> 
