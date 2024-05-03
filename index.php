9667e8f7205f72e0486970535bc5739c04a44544
Clatoeny <cla.toeny@hotmail.com>
2024-05-03 11:52:11 +0200

    boxes styling OF DOOOOOMMNESSS. fk spoti api

--- index.php
+++ index.php
@@ -4,26 +4,26 @@ require_once './backend/db/db.config.php';
 ?>
 
 <body>
-<main>
-    <div id="main-box" class="flex">
-        <div id="top-box" class="flex-1">
-            <div id="status-box" class="grid grid-rows-2 grid-cols-2 m-2 p-4 border border-gray-200 gap-2">
-                    <div id="weather-desc-box" class="col-span-2 bg-gray-200 p-2">Content 1A - Top</div>
-                    <div id="weather-icon-box" class="bg-gray-300 p-2">Content 1A - Bottom Left</div>
-                    <div id="flag-icon-box" class="bg-gray-300 p-2">Content 1A - Bottom Right</div>
-                </div>    
-                <div id="temp-box" class="m-2 p-4 border border-gray-200">Content 1B</div>
-            </div>
-            
-            <div id="bottom-box" class="flex-1">
-                <div id="play-box" class="m-2 p-4 border border-gray-200">Content 2A</div>
-                <div id="slider-box" class="m-2 p-4 border border-gray-200">Content 2B</div>
-            </div>
+<main class="p-[3%]">
+    <div id="main-box" class="flex flex-col gap-1 w-full">
+        <div id="top-box" class="flex flex-row h-[20vh] gap-1 w-full">
+            <div id="status-box" class="grid grid-rows-2 grid-cols-2 border border-gray-200 gap-1 w-full">
+                <div id="weather-desc-box" class="col-span-2 bg-gray-200">Sonnig</div>
+                <div id="weather-icon-box" class="bg-gray-300">Icon Wetter</div>
+                <div id="flag-icon-box" class="bg-gray-300">Flag</div>
+            </div>    
+            <div id="temp-box" class="border border-gray-200 w-full">temp</div>
+        </div>
+        
+        <div id="bottom-box" class="flex flex-row h-[10vh] gap-1 w-full">
+            <div id="play-box" class="border border-gray-200 w-full">click play</div>
+            <div id="slider-box" class="border border-gray-200 w-full">I'll be bar</div>
+        </div>
     </div>
-    <div id="spotify-container" class="m-2 p-4">
-        <!-- Placeholder for the Spotify iframe -->
-        <iframe id="spotify-iframe" src="https://open.spotify.com/embed/track/yourSpotifyTrackID" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <!-- Adjusted div for Spotify iframe -->
+    <div id="spotify-container" class="flex justify-center mx-auto w-full">
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4VN7J0uq62foOhZndwOegy?utm_source=generator&theme=0" width="100%" height="400" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
 </main>
 <?php require_once './frontend/components/footer.html'; ?>
-</body> 
+</body>
