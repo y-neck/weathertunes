2423f3b79fd908ff2d9ea4fb5042937e57de6aa9
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-03 09:10:59 +0200

    add demo_config for database connection

---
+++ backend/db/db.demo_config.php
@@ -0,0 +1,15 @@
+<?php
+// only for reference
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
