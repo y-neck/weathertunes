f89b167ae77f03dee7ac224f57e213c8da29960f
Clatoeny <cla.toeny@hotmail.com>
2024-05-03 14:57:04 +0200

    Boxen angepasst. pastweather box eingefügt. BÖÖÖX

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require_once './backend/db/db.config.php';
 ?>
 
 <body>
-<main class="p-[3%]">
-    <div id="main-box" class="flex flex-col gap-1 border border-gray-200 aspect-[3/2] w-full">
-        <div id="top-box" class="flex flex-row gap-1 w-full">
-            <div id="status-box" class="grid grid-rows-2 grid-cols-2 aspect-[1/1] border border-gray-200 gap-1 w-full">
+<main class="px-[6%] pt-[2%]">
+    <div id="main-box" class="flex flex-col gap-3 border border-gray-200 aspect-[3/2] w-full">
+        <div id="top-box" class="flex flex-row gap-3 w-full">
+            <div id="status-box" class="grid grid-rows-2 grid-cols-2 aspect-[1/1] border border-gray-200 gap-3 w-full">
                 <div id="weather-desc-box" class="col-span-2 rounded-lg bg-gray-200">Sonnig</div>
                 <div id="weather-icon-box" class="bg-gray-300 rounded-lg">Icon Wetter</div>
                 <div id="flag-icon-box" class="bg-gray-300 rounded-lg">Flag</div>
@@ -15,15 +15,17 @@ require_once './backend/db/db.config.php';
             <div id="temp-box" class="border border-gray-200 bg-gray-300 rounded-lg w-full">temp</div>
         </div>
         
-        <div id="bottom-box" class="flex flex-row h-[10vh] gap-1 w-full">
-            <div id="play-box" class="border bg-gray-300 border-gray-200 rounded-lg w-full">click play</div>
-            <div id="slider-box" class="border bg-gray-300 border-gray-200 rounded-lg w-full">I'll be bar</div>
+        <div id="bottom-box" class="flex flex-row h-[10vh] w-full">
+            <div id="play-button" class="border bg-gray-300 border-gray-200 rounded-lg w-full">click play</div>
+           
         </div>
     </div>
     <!-- Adjusted div for Spotify iframe -->
-    <div id="spotify-container" class="flex justify-center mx-auto w-full">
-        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4VN7J0uq62foOhZndwOegy?utm_source=generator&theme=0" width="100%" height="400" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <div id="spotify-container" class="flex justify-center m-2 mx-auto w-full">
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4VN7J0uq62foOhZndwOegy?utm_source=generator&theme=0" width="100%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
+    <div id="past-weather-box" class="flex flex-row h-[20vh] w-full">
+            <div id="play-box" class="border bg-gray-300 border-gray-200 rounded-lg w-full">click play</div>
 </main>
 <?php require_once './frontend/components/footer.html'; ?>
 </body>
