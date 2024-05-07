3891d2d6fdc3c56e786fc5d2b52717fb923f3a52
Clatoeny <cla.toeny@hotmail.com>
2024-05-07 13:24:00 +0200

    Past weather box formen of DESTINYYY

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once './backend/db/db.config.php';
 <main class="px-[6%] font-bold pt-[2%]">
     <div id="main-box" class="flex flex-col gap-3 aspect-[3/2] w-full">
         <div id="top-box" class="flex flex-row gap-3 w-full">
-            <div id="status-box" class="grid grid-rows-2 grid-cols-2 aspect-[1/1] gap-3 w-full">
+            <div id="status-box" class="grid grid-rows-2 grid-cols-2 aspect-[1/1] gap-1 w-full">
                 <div id="weather-desc-box" class="col-span-2 rounded-lg bg-gray-200">Sonnig</div>
                 <div id="weather-icon-box" class="bg-gray-300 rounded-lg">Icon Wetter</div>
                 <div id="flag-icon-box" class="bg-gray-300 rounded-lg">Flag</div>
@@ -24,8 +24,11 @@ require_once './backend/db/db.config.php';
     <div id="spotify-container" class="flex justify-center m-2 mx-auto w-full">
         <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4VN7J0uq62foOhZndwOegy?utm_source=generator&theme=0" width="100%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
-    <div id="past-weather-box" class="flex flex-row h-[20vh] w-full">
-            <div id="play-box" class="bg-gray-300 rounded-lg w-full">click play</div>
+
+    <div id="main-box" class="flex flex-col gap-0 w-full">
+                <div id="weather-desc-box" class="rounded-t-lg h-[10vh] bg-gray-200">Sonnig</div>
+                <div id="weather-icon-box" class="rounded-b-lg h-[10vh] bg-gray-300">Icon Wetter</div>
+    </div>
 </main>
 <?php require_once './frontend/components/footer.html'; ?>
 </body>
