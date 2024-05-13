4a64796c058b7662a00586b97fe9b70b6b30008f
joggiletti <joelschaller2000@gmail.com>
2024-05-13 14:54:42 +0200

    weather icon test finished

--- index.php
+++ index.php
@@ -10,36 +10,7 @@ require_once './backend/db/db.config.php';
             <div id="top-box" class="flex flex-row gap-3 w-full">
                 <div id="status-box" class="grid grid-rows-2 grid-cols-2 aspect-[1/1] gap-1 w-full">
                     <div id="weather-desc-box" class="col-span-2 rounded-lg bg-gray-200">Wetterbeschreibung</div>
-                    <div id="weather-icon-box" class="bg-gray-300 rounded-lg">
-                        <img src="../frontend/public/images/Icons.svg" alt="">
-                        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="1000" height="1000" viewBox="0 0 1000 1000">
-                            <defs>
-                            <style>
-                            .cls-1 {
-                                fill: none;
-                                stroke: #fff;
-                                stroke-linecap: round;
-                                stroke-miterlimit: 10;
-                                stroke-width: 50px;
-                            }
-
-                            .cls-2 {
-                                fill: #fff;
-                                stroke-width: 0px;
-                            }
-                            </style>
-                            </defs>
-                            <line class="cls-1" x1="685.57" y1="500" x2="813.32" y2="500"/>
-                            <line class="cls-1" x1="190.68" y1="500" x2="318.43" y2="500"/>
-                            <line class="cls-1" x1="502" y1="316.43" x2="502" y2="188.68"/>
-                            <line class="cls-1" x1="502" y1="811.32" x2="502" y2="683.57"/>
-                            <line class="cls-1" x1="631.8" y1="370.2" x2="722.13" y2="279.87"/>
-                            <line class="cls-1" x1="281.87" y1="720.13" x2="372.2" y2="629.8"/>
-                            <line class="cls-1" x1="372.2" y1="370.2" x2="281.87" y2="279.87"/>
-                            <path class="cls-2" d="M502,420.2c44,0,79.8,35.8,79.8,79.8s-35.8,79.8-79.8,79.8-79.8-35.8-79.8-79.8,35.8-79.8,79.8-79.8M502,370.2c-71.69,0-129.8,58.12-129.8,129.8s58.12,129.8,129.8,129.8,129.8-58.12,129.8-129.8-58.12-129.8-129.8-129.8h0Z"/>
-                            <line class="cls-1" x1="722.13" y1="720.13" x2="631.8" y2="629.8"/>
-                        </svg>
-                    </div>
+                    <div id="weather-icon-box" class="bg-gray-300 rounded-lg">Icon</div>
                     <div id="flag-icon-box" class="bg-gray-300 rounded-lg">Flag</div>
                 </div>
                 <div id="temp-box" class="bg-gray-300 rounded-lg w-full">temp</div>
