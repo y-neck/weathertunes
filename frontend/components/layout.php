acd54bf5af5afb485bf9c67356e2c7588d23b5ef
Clatoeny <cla.toeny@hotmail.com>
2024-05-14 10:16:45 +0200

    even more shadows

--- frontend/components/layout.php
+++ frontend/components/layout.php
@@ -30,7 +30,7 @@
                 },
                 extend: {
                     boxShadow: {
-                        'main': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+                        'main': '0 2px 15px -2px rgba(0, 0, 0, 0.2)',
                     }
                 }
             }
