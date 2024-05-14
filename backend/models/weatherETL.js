d82940a44aa4bb32911b25dbe5b6f6eb2f62cf81
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-14 16:10:13 +0200

    build case switch for weather icons

--- backend/models/weatherETL.js
+++ backend/models/weatherETL.js
@@ -21,12 +21,36 @@ try {
         })
         // Append to html
         .then(data => {
-            //console.log('Current Weather: ', data)
+            console.log('Current Weather: ', data)
             temperatureBox.innerHTML = `${data.temperature}°C`
             weatherDescriptionBox.innerHTML = data.weatherCodeDescription;
 
-            //windBox.innerHTML = data.windSpeed;
-            
+            // Case switch for weather icon
+            switch (data.weatherCodeDescription) {
+                case 'Klar':    // If weatherCodeDescription is 'Klar'
+                    if (data.isDay === 1) {
+                        weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/neg/Icons_Neg_Klar_Tag.png" alt="Klar" class="h-full w-auto p-[10px]">`;    // Display 'Klar' icon
+                    } else {
+                        weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/neg/Icons_Neg_Klar_Nacht.png" alt="Klar-Nacht" class="h-full w-auto p-[10px]">`;
+                    } break;
+                case 'Wulche':
+                    weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/neg/Icons_Neg_Wulche.png" alt="Wulche" class="h-full w-auto p-[10px]">`;
+                    break;
+                case 'Näbu':
+                    weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/neg/Icons_Neg_Naebu.png" alt="Näbu" class="h-full w-auto p-[10px]">`;
+                    break;
+                case 'Nisu':
+                    weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/neg/Icons_Neg_Nisu.png" alt="Nisu" class="h-full w-auto p-[10px]">`;
+                    break;
+                case 'Räge':
+                    weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/neg/Icons_Neg_Raege.png" alt="Räge" class="h-full w-auto p-[10px]">`;
+                    break;
+                case 'Schnee':
+                    weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/pos/Icons_Pos_Schnee.png" alt="Schnee" class="h-full w-auto p-[10px]">`;
+                    break;
+                case 'Sturm':
+                    weatherIconBox.innerHTML = `<img src="frontend/public/img/weather_icons/neg/Icons_Neg_Sturm.png" alt="Sturm" class="h-full w-auto p-[10px]">`;
+            }
         })
 }
 catch (error) {
