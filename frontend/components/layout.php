6f1f14e6edab86d26a79afeb1041f1d8584338c2
Clatoeny <cla.toeny@hotmail.com>
2024-05-14 09:54:07 +0200

    farben done

--- frontend/components/layout.php
+++ frontend/components/layout.php
@@ -30,7 +30,7 @@
                 },
                 extend: {
                     boxShadow: {
-                        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+                        'main': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
                     }
                 }
             }
