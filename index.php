76d0d151f36d916ed3fa137496d1ca7f755cf786
Clatoeny <cla.toeny@hotmail.com>
2024-05-14 14:37:59 +0200

    irgendöppis

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ require_once './backend/db/db.config.php';
         <div id="main-box" class="flex flex-col gap-5 mb-5 aspect-[3/2] w-full">
             <div id="top-box" class="flex flex-row gap-5 w-full">
                 <div id="status-box" class="grid grid-rows-2 grid-cols-2 aspect-[1/1] text-moButton gap-2 w-full">
-                    <div id="weather-desc-box" class="flex justify-center items-center col-span-2 shadow-main rounded-lg bg-middle">NÄBU</div>
+                    <div id="weather-desc-box" class="flex justify-center items-center col-span-2 shadow-main rounded-lg bg-middle"></div>
                     <div id="weather-icon-box" class="flex justify-center items-center bg-middle shadow-main rounded-lg">
-                        <img src="frontend/public/img/weather_icons/neg/Icons_Neg_Naebu.png" alt="Weather Icon" class="h-full w-auto"> 
+                        <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Naebu.png" alt="NÄBU" class="h-full w-auto"> 
                     </div>
                     <div id="flag-icon-box" class="flex justify-center items-center bg-middle shadow-main rounded-lg">Flag</div>
                 </div>
@@ -25,13 +25,18 @@ require_once './backend/db/db.config.php';
             </div>
         </div>
         <!-- Adjusted div for Spotify iframe -->
-        <div id="spotify-container" class="flex justify-center m-2 mx-auto w-full">
+        <div id="spotify-container" class="flex justify-center mb-5 m-2 mx-auto w-full">
             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4VN7J0uq62foOhZndwOegy?utm_source=generator&theme=0" width="100%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
 
         <div id="past-weather-box" class="flex flex-col gap-0 w-full mb-2">
             <div id="past-weather-text-box" class="flex justify-center items-center rounded-t-lg h-[10vh] text-moButton shadow-main bg-middle">WETTERRÜCKLICK</div>
-            <div id="past-weather-icon-box" class="flex justify-center items-center rounded-b-lg h-[10vh] shadow-main bg-dark">Icon Wetter</div>
+            <div id="past-weather-icon-box" class="flex justify-center items-center rounded-b-lg h-[10vh] text-moButton shadow-main bg-dark">
+                    - <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Naebu.png" alt="NÄBU" class="h-12 w-auto">
+                    - <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Naebu.png" alt="NÄBU" class="h-12 w-auto">
+                    - <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Naebu.png" alt="NÄBU" class="h-12 w-auto">
+                    ->
+            </div>
         </div>
     </main>
     <?php require_once './frontend/components/footer.html'; ?>
