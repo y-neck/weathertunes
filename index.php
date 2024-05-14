669871140193a743990ec4d1a098c071d6672e2e
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-14 15:09:27 +0200

    update placeholders

--- index.php
+++ index.php
@@ -10,13 +10,13 @@ require_once './backend/db/db.config.php';
         <div id="main-box" class="flex flex-col gap-5 mb-5 aspect-[3/2] w-full">
             <div id="top-box" class="flex flex-row gap-5 w-full">
                 <div id="status-box" class="grid grid-rows-2 grid-cols-2 aspect-[1/1] text-moButton gap-2 w-full">
-                    <div id="weather-desc-box" class="flex justify-center items-center col-span-2 shadow-main rounded-lg bg-middle"></div>
+                    <div id="weather-desc-box" class="flex justify-center items-center col-span-2 shadow-main rounded-lg bg-middle">Wätterlag</div>
                     <div id="weather-icon-box" class="flex justify-center items-center bg-middle shadow-main rounded-lg">
-                        <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Naebu.png" alt="NÄBU" class="h-full w-auto"> 
+                        <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Naebu.png" alt="Wätter-Icon" class="h-full w-auto">
                     </div>
-                    <div id="flag-icon-box" class="flex justify-center items-center bg-middle shadow-main rounded-lg">Flag</div>
+                    <div id="flag-icon-box" class="flex justify-center items-center bg-middle shadow-main rounded-lg">Windstärchi</div>
                 </div>
-                <div id="temp-box" class="flex justify-center items-center text-moTemp bg-dark shadow-main rounded-main rounded-lg w-full">temp</div>
+                <div id="temp-box" class="flex justify-center items-center text-moTemp bg-dark shadow-main rounded-main rounded-lg w-full">Temperatur</div>
             </div>
 
             <div id="bottom-box" class="flex flex-row h-[10vh] w-full">
