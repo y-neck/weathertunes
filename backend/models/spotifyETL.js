7d29c79c47df147ec15f90f835a863d68bad7571
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-15 17:07:18 +0200

    get html elements

---
+++ backend/models/spotifyETL.js
@@ -0,0 +1,7 @@
+const spotifyParametersUrl = './backend/db/03_unloadSpotify.php'
+
+// Get html elements
+const tuneInBtn = document.querySelector('#play-button');
+const player = document.querySelector('#spotify-container');
+
+
