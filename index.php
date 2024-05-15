9d05257827f36ba06672f714b0f18b9b2515fee5
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-15 17:52:37 +0200

    div to button

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ require_once './backend/db/db.config.php';
             </div>
 
             <div id="bottom-box" class="flex flex-row h-[10vh] w-full">
-                <div id="play-button" class="flex justify-center items-center bg-text shadow-main text-moButton text-dark rounded-lg w-full">TUNE IN</div>
+                <button id="play-button" class="flex justify-center items-center bg-text shadow-main text-moButton text-dark rounded-lg w-full">TUNE IN</button>
 
             </div>
         </div>
