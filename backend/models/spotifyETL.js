53697181db7cffc2a67ab452eaffe7fe1942c9ec
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-15 17:08:25 +0200

    fetch spotify parameter data

--- backend/models/spotifyETL.js
+++ backend/models/spotifyETL.js
@@ -4,4 +4,25 @@ const spotifyParametersUrl = './backend/db/03_unloadSpotify.php'
 const tuneInBtn = document.querySelector('#play-button');
 const player = document.querySelector('#spotify-container');
 
+// Fetch parameter data
+try {
+    fetch(spotifyParametersUrl)
+        .then(response => {
+            // Check if the response is successful (status code 200)
+            if (!response.ok) {
+                throw new Error(Error);
+            }
+            // Parse the JSON response
+            return response.json();
+        })
+        .then(data => {
+            console.log('Spotify Parameters: ', data);
+        })
+} catch (error) {
+    console.log(error)
+}
 
+// Event listener for tune in button
+tuneInBtn.addEventListener('click', () => {
+    console.log(data);
+})
