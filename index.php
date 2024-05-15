e4ae31b207c3305f41d39ed4bed8cbbee98a5430
Clatoeny <cla.toeny@hotmail.com>
2024-05-15 14:34:53 +0200

    Things

--- index.php
+++ index.php
@@ -35,10 +35,10 @@ require_once './backend/db/db.config.php';
             <div id="past-weather-icon-box" class="flex justify-start mb-5 items-center rounded-b-lg h-[10vh] shadow-main bg-dark">
                     <img src="frontend/public/img/standard_icons/timeline.svg" alt="Past Weather Icon" class="h-auto w-[95%]">
                 </div>
-                <div class="placeholder-container absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                    <img src="frontend/public/img/standard_icons/placeholder1.png" alt="Placeholder 1" class="h-auto w-[20%] mx-2">
-                    <img src="frontend/public/img/standard_icons/placeholder2.png" alt="Placeholder 2" class="h-auto w-[20%] mx-2">
-                    <img src="frontend/public/img/standard_icons/placeholder3.png" alt="Placeholder 3" class="h-auto w-[20%] mx-2">
+                <div class="placeholder-container absolut top-0 left-0 w-full h-full flex justify-center items-center">
+                    <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Klar_Nacht.png" alt="Placeholder 1" class="h-[60%] w-auto mx-2">
+                    <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Klar_Nacht.png" alt="Placeholder 2" class="h-[60%] w-auto mx-2">
+                    <img src="frontend\public\img\weather_icons\neg\Icons_Neg_Klar_Nacht.png" alt="Placeholder 3" class="h-[60%] w-auto mx-2">
                 </div>
             </div>
         </div>
