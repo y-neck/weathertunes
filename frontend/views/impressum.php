4afec8d7f83cc33bd0e193eb68b9a055820e8880
Clatoeny <cla.toeny@hotmail.com>
2024-05-20 16:16:18 +0200

    impressum styling

--- frontend/views/impressum.php
+++ frontend/views/impressum.php
@@ -15,15 +15,15 @@ require_once '../components/layout.php';
             Entwickelt und gestaltet wurde Weathertunes von Cla Töny, Joél Schaller und Yannick Spriessler im Rahmen ihres Bachelorstudiums 
             in Multimedia Productions an der FHGR und BFH. Mehr Informationen findest du hier.
         </p>
-        <div class="mt-8">
+        <div class="mt-8 leading-none">
             <h2 class="text-2xl font-semibold text-text mb-2">Master Frontend</h2>
-            <p class="text-gray-700 mb-6">Cla Töny</p>
+            <p class="text-text mb-6">Cla Töny</p>
 
             <h2 class="text-2xl font-semibold text-text mb-2">Head of Design</h2>
-            <p class="text-gray-700 mb-6">Joél Schaller</p>
+            <p class="text-text mb-6">Joél Schaller</p>
 
             <h2 class="text-2xl font-semibold text-text mb-2">Backend Magician</h2>
-            <p class="text-gray-700">Yannick Spriessler</p>
+            <p class="text-text">Yannick Spriessler</p>
         </div>
     </div>
 
