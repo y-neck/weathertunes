bca1e9f8d77adbe13a59989368451fd2d8c6ea5d
Clatoeny <cla.toeny@hotmail.com>
2024-05-20 16:28:08 +0200

    Impressum done - footer

--- frontend/views/impressum.php
+++ frontend/views/impressum.php
@@ -13,10 +13,10 @@ require_once '../components/layout.php';
             <img src="..\public\img\groupphoto\PH-gruppenfoto.jpg" alt="Group Picture" class="rounded-lg shadow-lg w-full max-w-4xl">
         </div>
     </div>
-    <div class="container mx-auto mt-4 p-6 bg-bg">
+    <div class="container mx-auto mt-0 p-4 bg-bg">
         <p class="text-text mb-4">
             Entwickelt und gestaltet wurde Weathertunes von Cla Töny, Joél Schaller und Yannick Spriessler im Rahmen ihres Bachelorstudiums 
-            in Multimedia Productions an der FHGR und BFH. Mehr Informationen findest du hier.
+            in Multimedia Productions an der FHGR und BFH. Mehr Informationen findest du <a href="https://www.fhgr.ch/en/" class="underline">hier</a>.
         </p>
         <div class="mt-8 leading-none">
             <h2 class="text-2xl font-semibold text-text mb-2">Master Frontend</h2>
