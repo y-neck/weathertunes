749e3df607307741f4fcdcd724bc806d8705b5c8
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-20 16:51:50 +0200

    add /div

--- frontend/views/impressum.php
+++ frontend/views/impressum.php
@@ -13,22 +13,23 @@ require_once '../components/layout.php';
             <div class="container mx-auto flex justify-center">
                 <img src="..\public\img\groupphoto\PH-gruppenfoto.jpg" alt="Group Picture" class="rounded-lg shadow-lg w-full max-w-4xl">
             </div>
-            <div class="container mx-auto mt-0 p-4 bg-background flex flex-col text-left">
-                <p class="text-text mb-4">
-                    Entwickelt und gestaltet wurde Weathertunes von Cla Töny, Joél Schaller und Yannick Spriessler im Rahmen ihres Bachelorstudiums
-                    in Multimedia Productions an der FHGR und BFH. Mehr Informationen findest du <a href="https://www.fhgr.ch/en/" class="underline">hier</a>.
-                </p>
-                <div class="mt-8 leading-none">
-                    <h2 class="text-2xl font-semibold text-text mb-2">Master Frontend</h2>
-                    <p class="text-text mb-6">Cla Töny</p>
+        </div>
+        <div class="container mx-auto mt-0 p-4 bg-background flex flex-col text-left">
+            <p class="text-text mb-4">
+                Entwickelt und gestaltet wurde Weathertunes von Cla Töny, Joél Schaller und Yannick Spriessler im Rahmen ihres Bachelorstudiums
+                in Multimedia Productions an der FHGR und BFH. Mehr Informationen findest du <a href="https://www.fhgr.ch/en/" class="underline">hier</a>.
+            </p>
+            <div class="mt-8 leading-none">
+                <h2 class="text-2xl font-semibold text-text mb-2">Master Frontend</h2>
+                <p class="text-text mb-6">Cla Töny</p>
 
-                    <h2 class="text-2xl font-semibold text-text mb-2">Head of Design</h2>
-                    <p class="text-text mb-6">Joél Schaller</p>
+                <h2 class="text-2xl font-semibold text-text mb-2">Head of Design</h2>
+                <p class="text-text mb-6">Joél Schaller</p>
 
-                    <h2 class="text-2xl font-semibold text-text mb-2">Backend Magician</h2>
-                    <p class="text-text">Yannick Spriessler</p>
-                </div>
+                <h2 class="text-2xl font-semibold text-text mb-2">Backend Magician</h2>
+                <p class="text-text">Yannick Spriessler</p>
             </div>
+        </div>
     </main>
 </body>
 <?php require_once '../components/footer.html'; ?>
