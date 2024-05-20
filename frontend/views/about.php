cce25cec031953c1378e6eb9b2b259278c283cb8
Clatoeny <cla.toeny@hotmail.com>
2024-05-20 17:00:52 +0200

    tabelle?

--- frontend/views/about.php
+++ frontend/views/about.php
@@ -6,7 +6,45 @@ require_once '../components/layout.php';
 
 <body class="bg-background">
     <main class="w-full bg-background p-4">
-
+    <div class="overflow-x-auto">
+    <table class="min-w-full bg-white">
+        <thead>
+            <tr>
+                <th class="px-4 py-2 border">Music Style</th>
+                <th class="px-4 py-2 border">Klar</th>
+                <th class="px-4 py-2 border">Wulche</th>
+                <th class="px-4 py-2 border">Nääbu</th>
+                <th class="px-4 py-2 border">Nisu</th>
+                <th class="px-4 py-2 border">Rääge</th>
+                <th class="px-4 py-2 border">Schnee</th>
+                <th class="px-4 py-2 border">Sturm</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="px-4 py-2 border">Pop</td>
+                <td class="px-4 py-2 border bg-yellow-500"></td>
+                <td class="px-4 py-2 border bg-yellow-500"></td>
+                <td class="px-4 py-2 border bg-yellow-500"></td>
+                <td class="px-4 py-2 border"></td>
+                <td class="px-4 py-2 border"></td>
+                <td class="px-4 py-2 border"></td>
+                <td class="px-4 py-2 border"></td>
+            </tr>
+            <tr>
+                <td class="px-4 py-2 border">Reggae</td>
+                <td class="px-4 py-2 border bg-yellow-500"></td>
+                <td class="px-4 py-2 border bg-yellow-500"></td>
+                <td class="px-4 py-2 border bg-yellow-500"></td>
+                <td class="px-4 py-2 border"></td>
+                <td class="px-4 py-2 border"></td>
+                <td class="px-4 py-2 border"></td>
+                <td class="px-4 py-2 border"></td>
+            </tr>
+            <!-- Continue for all music styles -->
+        </tbody>
+    </table>
+</div>
 
 
     
