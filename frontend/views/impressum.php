56d8c51b082ea54a0bc105ee0f103e892df75d86
Clatoeny <cla.toeny@hotmail.com>
2024-05-21 11:15:15 +0200

    stuff apasst margins

--- frontend/views/impressum.php
+++ frontend/views/impressum.php
@@ -8,20 +8,20 @@ require_once '../components/layout.php';
     <main class="w-full bg-bg px-4 flex flex-col">
         <div>
             <div class="container mx-auto text-left">
-                <h1 class="text-4xl font-bold text-text mb-4">Impressum</h1>
+                <h1 class="text-4xl font-bold text-text">Impressum</h1>
             </div>
             <div class="container mx-auto flex justify-center">
                 <img src="..\public\img\groupphoto\PH-gruppenfoto.jpg" alt="Group Picture" class="rounded-lg shadow-lg w-full max-w-4xl">
             </div>
 
             <div class="container mx-auto mt-0 p-4 bg-background flex flex-col text-left">
-                <p class="text-text mb-4">
+                <p class="text-text">
                     Entwickelt und gestaltet wurde Weathertunes von Cla Töny, Joél Schaller und Yannick Spriessler im Rahmen ihres Bachelorstudiums
                     in Multimedia Productions an der FHGR und BFH. Mehr Informationen findest du <a href="/human.txt" class="underline">hier</a>.
                 </p>
             </div>
         </div>
-        <div class="mt-8 leading-none">
+        <div class="mt-8 leading-none mb-4">
             <h2 class="text-2xl font-semibold text-text mb-2">Master of Frontend</h2>
             <p class="text-text mb-6">Cla Töny</p>
 
