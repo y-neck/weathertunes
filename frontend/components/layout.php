1f648be0a3c429bacd398305849e1bfc2fe7ac39
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-05-25 16:13:50 +0200

    comments

--- frontend/components/layout.php
+++ frontend/components/layout.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="author" href="humans.txt">
 
-    <!--TODO: Tab image-->
+    <!--Tab image-->
     <link rel="icon" href="frontend/public/img/favicon/Favicon.ico">
 
     <!-- Tailwind Config -->
