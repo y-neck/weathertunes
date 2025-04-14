84008a7c718ae8387dc5a70cc4df30944537576e
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-04-14 22:38:11 +0200

    feat: add layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
-  <div
-    class="border-box m-4 mx-auto flex h-screen w-screen flex-col md:m-[148px]"
-  >
+  <div class="border-box flex h-screen w-full flex-col p-4 md:p-[148px]">
     <slot />
+    <Footer class="mt-auto" />
   </div>
 </template>
