fd82d60342abd2f15118bd3e3c5c1cf0e1248316
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-04-14 22:40:19 +0200

    style: footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,27 +1,40 @@
 <template>
-  <footer class="bg-background text-white ">
-    <div class="container mx-auto flex gap-1 justify-between w-full border-box">
-        <NuxtLink to="/impressum" class="footer-link">Impressum</NuxtLink>
-        <NuxtLink to="/datenschutz" class="footer-link">Datenschutz</NuxtLink>
-        <NuxtLink to="https://github.com/y-neck/weathertunes" target="_blank" class="footer-link">
-            <img src="~/public/img/standard_icons/github-mark-pos.png" alt="GitHub" class="footer-icon-h-full w-auto">
-        </NuxtLink>
-        <NuxtLink to="/fireplace.vue" class="footer-link">
-            <img src="~/public/img/standard_icons/Fireplace_pos_Mug.png" alt="Fireplace" class="footer-icon-h-full w-auto">
-        </NuxtLink>
-        <NuxtLink to="/about.vue" class="footer-link"></NuxtLink>
+  <footer class="bg-background text-white">
+    <div class="border-box flex justify-between gap-1">
+      <NuxtLink to="/impressum" class="footer-link"
+        >Impressum <br />
+        Datenschutz</NuxtLink
+      >
+      <NuxtLink
+        to="https://github.com/y-neck/weathertunes"
+        target="_blank"
+        class="footer-link"
+      >
+        <img
+          src="~/public/img/standard_icons/github-mark-pos.png"
+          alt="GitHub"
+          class="footer-icon h-full w-auto p-2"
+        />
+      </NuxtLink>
+      <NuxtLink to="/fireplace.vue" class="footer-link">
+        <img
+          src="~/public/img/standard_icons/Fireplace_pos_Mug.png"
+          alt="Fireplace"
+          class="footer-icon h-full w-auto p-2"
+        />
+      </NuxtLink>
+      <NuxtLink to="/about.vue" class="footer-link">About</NuxtLink>
     </div>
   </footer>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
-.footer-link{
-  @apply bg-[#f2f2f2] text-black decoration-none border-text border-2 h-10 w-full flex align-center justify-center cursor-pointer gap-2.5 rounded-md;
+.footer-link {
+  @apply flex h-[48px] w-full cursor-pointer items-center justify-center gap-[16px] rounded-[16px] bg-[#f2f2f2] text-center text-[#000];
 }
 .hover\\:text-gray-300:hover {
-    color: #E0E0E0;
-    }
+  color: #e0e0e0;
+}
 </style>
